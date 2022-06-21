0f42c04db7af5fdebc15014267c7d0183841b77d
Sam Morris <sam@robots.org.uk>
2022-06-21 11:51:16 +0100

    Warn if client keytab can't be read

--- hitron_exporter/ipavault.py
+++ hitron_exporter/ipavault.py
@@ -1,12 +1,19 @@
+from logging import getLogger
+import os
 from typing import Sequence
 
 from ipalib import api
+
+
+LOGGER = getLogger(__name__)
 
 
 api_finalized = False
 
 
 def retrieve(vault_namespace):
+    check_keytab_readable()
+
     maybe_finalize_api()
 
     api.Backend.rpcclient.connect()
@@ -17,6 +24,21 @@ def retrieve(vault_namespace):
         }
     finally:
         api.Backend.rpcclient.disconnect()
+
+
+def check_keytab_readable():
+    breakpoint()
+    if 'KRB5_CLIENT_KTNAME' not in os.environ:
+        return
+
+    # MIT Kerberos doesn't log any errors if the keytab isn't accessible. This
+    # check isn't perfect, because it won't catch the case where the client
+    # keytab is in the default location & this environment variable is unset.
+    try:
+        with open(os.environ['KRB5_CLIENT_KTNAME']) as _:
+            pass
+    except Exception:
+        LOGGER.exception('The client keytab %r is not readable; we will not be able to retrieve credentials from FreeIPA', os.environ['KRB5_CLIENT_KTNAME'])
 
 
 def maybe_finalize_api():
