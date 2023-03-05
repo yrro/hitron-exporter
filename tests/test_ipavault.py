dd12b0dbdfd1144e16ec0ae8c96245a0e0bafab8
Sam Morris <sam@robots.org.uk>
2023-03-05 18:44:54 +0000

    test_ipavault: more compact Mock creation

--- tests/test_ipavault.py
+++ tests/test_ipavault.py
@@ -13,10 +13,8 @@ from hitron_exporter import ipavault
 def test_retrieve_ok(monkeypatch):
     # given:
     expected = json.dumps({"a": 1, "b": 2})
-    mock_completed = mock.Mock()
-    mock_completed.stdout = json.dumps(expected)
-    mock_run = mock.Mock(return_value=mock_completed)
-    monkeypatch.setattr("subprocess.run", mock_run)
+    mock_completed = mock.Mock(stdout=json.dumps(expected))
+    monkeypatch.setattr("subprocess.run", mock.Mock(return_value=mock_completed))
 
     # when:
     data = ipavault.retrieve(["service", "HTTP/blah.example.com"])
